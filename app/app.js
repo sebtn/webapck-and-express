d382087ba5ca77285b010e7f61b3b719ce957efc
Seb <seb@debian>
2017-06-01 13:16:54 -0400

    Fixed typo

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
-import React, { Componenet } from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App/>, document.getElementById('root`'))
+ReactDOM.render(<App/>, document.getElementById('root'))
 
