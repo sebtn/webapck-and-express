2ab788b5174e86f181b3dde54ef5754d94c5b67c
sebtn <sdebedout@gmail.com>
2017-06-03 15:21:09 -0400

    Add SCSS support and devtools for debugger

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,32 @@ let path = require('path')
 let webpack = require('webpack')
 
 module.exports = {
-	entry: {
-		path: ['./app/app.js']
-	},
-	output: {
-		path:path.resolve( __dirname, 'public'),
-		filename: 'bundle.js',
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js|.jsx?$/,
-				exclude:/node_modules/,
-				loader: 'babel-loader',
-				query: {
-					presets: ["react", "babel-preset-stage-0", "babel-preset-es2015"]
-				}
-			}
-		]
-	}
+  entry: [
+    './app/app.js',
+  ],
+  output: {
+    path: path.resolve( __dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    alias: {
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js|.jsx?$/,
+        exclude:/node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ["babel-preset-es2015", "react", "babel-preset-stage-0" ]
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
 }
